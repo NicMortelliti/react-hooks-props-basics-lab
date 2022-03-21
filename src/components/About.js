474f53cacf66a092330823d6073548995db06b9a
NicMortelliti <npmortelliti@gmail.com>
2022-03-21 14:46:59 -0700

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,11 @@ function About(props) {
     <div id="about">
       <h2>About Me</h2>
 
+      {/* Hide the p tag if no bio is supplied */}
+      {props.bio ? <p>{props.bio}</p> : null}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links links={props.links} />
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
